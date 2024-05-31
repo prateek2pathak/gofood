1ac0ac18a26ba99cf02caa869dd6a1577f087d64
Prateek Pathak <119005608+prateek2pathak@users.noreply.github.com>
2024-05-31 09:20:09 +0530

    Update db.js

--- backend/database/db.js
+++ backend/database/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-const databaseUrl = "mongodb+srv://prateekpathak042:<password>@cluster0.iiynm78.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster0";
+const databaseUrl = "mongodb+srv://prateekpathak042:prateek2004@cluster0.iiynm78.mongodb.net/gofood?retryWrites=true&w=majority&appName=Cluster0";
 
 const connectToDB = async () => {
   try {
